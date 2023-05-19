a03d534943dae24c4e8f41fede269529558fa163
mohammedadel939 <134050456+mohammedadel939@users.noreply.github.com>
2023-05-19 16:29:07 +0300

    Delete portfolio1.9 directory

--- portfolio1.9/js/global.js
+++
@@ -1,8 +0,0 @@
-let toTop = document.querySelector(".toTop");
-window.addEventListener("scroll", function () {
-  if (this.scrollY > 1000) {
-    toTop.style.right = "30px";
-  } else {
-    toTop.style.right = "-60px";
-  }
-});
